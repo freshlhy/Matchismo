a566a68f86cc768b3f2ed71acf4a7469db35edf7
freshlhy <freshlhy@gmail.com>
2013-12-18 07:57:22 -0800

    finish game

--- Matchismo/Model/CardMatchingGame.m
+++ Matchismo/Model/CardMatchingGame.m
@@ -48,6 +48,8 @@
 }
 
 static const int MISMATCH_PENALTY = 2;
+static const int MATCH_BONUS = 4;
+static const int COST_TO_CHOOSE = 1;
 
 - (void)chooseCardAtIndex:(NSUInteger)index
 {
@@ -62,12 +64,17 @@ static const int MISMATCH_PENALTY = 2;
                 if (otherCard.isChosen && !otherCard.isMatched) {
                     int matchScore = [card match:@[otherCard]];
                     if (matchScore) {
-                        self.score += matchScore;
+                        self.score += matchScore * MATCH_BONUS;
+                        otherCard.matched = YES;
+                        card.matched = YES;
                     } else {
                         self.score -= MISMATCH_PENALTY;
+                        otherCard.chosen = NO;
                     }
+                    break;
                 }
             }
+            self.score -= COST_TO_CHOOSE;
             card.chosen = YES;
         }
     }
