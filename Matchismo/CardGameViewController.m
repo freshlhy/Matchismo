8f05eb63340017d8cd72761bf104a76d1c97268c
freshlhy <freshlhy@gmail.com>
2013-12-18 01:04:03 -0800

    card matching game ...

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -7,13 +7,26 @@
 //
 
 #import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
 
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
+@property (strong, nonatomic) Deck *deck;
 @end
 
 @implementation CardGameViewController
+
+- (Deck *)deck
+{
+    if (!_deck) _deck = [self createDeck];
+    return _deck;
+}
+
+- (Deck *)createDeck
+{
+    return [[PlayingCardDeck alloc] init];
+}
 
 - (void)setFlipCount:(int)flipCount
 {
@@ -28,9 +41,12 @@
                           forState:UIControlStateNormal];
         [sender setTitle:@"" forState:UIControlStateNormal];
     } else {
-        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
-                          forState:UIControlStateNormal];
-        [sender setTitle:@"A♣︎" forState:UIControlStateNormal];
+        Card *randomCard = [self.deck drawRandomCard];
+        if (randomCard) {
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                              forState:UIControlStateNormal];
+            [sender setTitle:randomCard.contents forState:UIControlStateNormal];
+        }
     }
     self.flipCount++;
 }
---
+++ Matchismo/Model/CardMatchingGame.m
@@ -0,0 +1,88 @@
+//
+//  CardMatchingGame.m
+//  Matchismo
+//
+//  Created by freshlhy on 12/17/13.
+//  Copyright (c) 2013 CS193p. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@interface CardMatchingGame()
+@property (nonatomic, readwrite) NSUInteger score;
+@property (nonatomic, strong) NSMutableArray *cards;
+@end
+
+@implementation CardMatchingGame
+
+- (NSMutableArray *)cards
+{
+    if (!_cards) _cards = [[NSMutableArray alloc] init];
+    return _cards;
+}
+
+- (instancetype)initWithCardCount:(NSUInteger)count
+                        usingDeck:(Deck *)deck   {
+    self = [super init]; // super's designated initializer
+
+    
+    if (self) {
+        for (int i = 0; i < count; i++) {
+            Card *randomCard = [deck drawRandomCard];
+            if (randomCard) {
+                [self.cards addObject:randomCard];
+            } else {
+                self = nil;
+                break;
+            }
+
+        }
+    }
+    
+    return self;
+}
+
+- (Card *)cardAtIndex:(NSUInteger)index
+{
+    return (index < [self.cards count]) ? self.cards[index] : nil;
+}
+
+static const int MISMATCH_PENALTY = 2;
+
+- (void)chooseCardAtIndex:(NSUInteger)index
+{
+    Card *card = [self cardAtIndex:index];
+    
+    if (!card.isMatched) {
+        if (card.isChosen) {
+            card.chosen = NO;
+        } else {
+            // match against other chosen cards
+            for (Card *otherCard in self.cards) {
+                if (otherCard.isChosen && !otherCard.isMatched) {
+                    int matchScore = [card match:@[otherCard]];
+                    if (matchScore) {
+                        self.score += matchScore;
+                    } else {
+                        self.score -= MISMATCH_PENALTY;
+                    }
+                }
+            }
+            card.chosen = YES;
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+@end
